ec095233910641751ad12d4a72d6dc1ccf80d15b
Costache Robert Valentin <costache.robert.valentin@gmail.com>
2022-11-28 12:46:54 +0100

    List Updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from 'react';
+import Fragment  from 'react'
 
 function App() {
   // Counter useState
@@ -8,11 +9,14 @@ function App() {
   // Toggle Theme useState
   const [theme, setTheme] = useState('light');
 
+  // Added the array element
+  const numbersList = [<li>1</li>, <li>2</li>, <li>3</li>, <li>4</li>];
+  const students = ['Marc', 'Lucy', 'Anna'];
+
   return (
     <>  
     <div className='Counter'>
       <h2>Counter</h2>
-      
       <p>You clicked {count} times!</p>
       <button onClick={( ) => setCount(count  - 1)}>-</button>
       <button onClick={( ) => setCount(count  + 1)}>+</button>
@@ -26,6 +30,22 @@ function App() {
         <option value="light"> Light </option>
         <option value="dark"> Dark </option>
       </select>
+    </div>
+
+    {/* Add students list with map function */}
+    <div className="list">
+      <h2>List of students</h2>
+      { students.map((name, index) => {
+        return <p key={index} > { name } </p>
+      })}
+
+      {/* List of numbers */}
+      <h2>List of students</h2>
+      <ul>
+        { numbersList.map((list, index, array) => {
+          return  <span key={index}>{list}</span>
+        })}
+      </ul>
     </div>
     </>
 
